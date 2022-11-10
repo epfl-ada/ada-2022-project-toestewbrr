639dabcfd500241b79b2bf3c7583e8fe2264ba13
Antoine Bonnet <abonnet@tsf-492-wpa-0-121.epfl.ch>
2022-11-10 13:45:03 +0100

    Refactoring files

---
+++ word_analysis.py
@@ -0,0 +1,6 @@
+''' 
+Applied Data Analysis @ EPFL
+Team: ToeStewBrr - Alexander Sternfeld, Marguerite Thery, Antoine Bonnet, Hugo Bordereaux 
+Project: Love stories in movies
+Dataset: CMU Movie Summary Corpus
+'''
