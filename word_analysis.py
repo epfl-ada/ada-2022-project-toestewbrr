8f3f6d6dc6f521d277cbff84993a0169a87c6dff
Antoine Bonnet <abonnet04@gmail.com>
2022-11-10 13:55:03 +0100

    Delete word_analysis.py

--- word_analysis.py
+++
@@ -1,6 +0,0 @@
-''' 
-Applied Data Analysis @ EPFL
-Team: ToeStewBrr - Alexander Sternfeld, Marguerite Thery, Antoine Bonnet, Hugo Bordereaux 
-Project: Love stories in movies
-Dataset: CMU Movie Summary Corpus
-'''
